d8207c93f0733e751085f5c05304cae159aac08a
Joseph Tenore <jtenore1@massasoit.mass.edu>
2023-03-03 13:22:15 -0500

    certificate page

--- src/components/Certificate.js
+++ src/components/Certificate.js
@@ -4,11 +4,40 @@ export const Certificate = () => {
 	return (
 		<div>
 			<h1>
-				<a
-					href="https://coursera.org/share/e72cce09b672732611e668d942d2f9e0"
-					title="Check out my online Certificate from google"
-				>Google IT Certificate</a>
+				Completed
 			</h1>
+			<br/>
+			<h2>
+				<a href="https://coursera.org/share/e72cce09b672732611e668d942d2f9e0" title="Check out my online Certificate from google">Google IT Certificate</a>
+			</h2>
+			<p>
+				The program took a lot of effort and taught so much including
+					<li>Technical Support Fundamentals</li>
+					<li>The in depth leasons on computer networking</li>
+					<li>Operating Systems and got in depth on th CLI on both linux and windows</li>
+					<li>Covered many areas of System admin and IT infrustructures and many IT tools and software</li>
+					<li>Wide knowledge of Computer security and security protocols and encryption software</li>
+			</p>
+			<br/>
+			<br/>
+			<h1>
+				In Progress
+			</h1>
+			<br/>
+			<h2>
+				Comptia A+
+			</h2>
+			<p>
+				I've been working on my A+ for a while now an am getting comfortable enough to take the exams already scheduled the first one and will soon update hopefully.
+			</p>
+			<h2>
+				<br/>
+				Not Started
+				<br/>
+				Comptia Linux+
+				<br/>
+				Comptia Network+
+			</h2>
 		</div>
 	);
 };
