885d5c4567a1107451d03794e210afea27d656e7
Joseph Tenore <jtenore1@massasoit.mass.edu>
2023-03-06 12:17:55 -0500

    Fixed a lot of the spacing and fixed up some of the content on the Resume Page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -25,40 +25,43 @@ export const Resume = () => {
 			</div>
 
 			<h1 className="top_pad, res_header2">Skills</h1>
+			<h2>Computer Languages: Javascript/React, Ruby, HTML, CSS, SQL, Python, Java, BASH, CLI</h2>
 
 			<div className="skill_box">
 				<div>
 					<ul>
+						<li>Worked with Windows/MacOS/Linux</li>
+						<li>Software and Hardware Troubleshooting</li>
 						<li>I have built my own PC</li>
-						<li>JavaScript/React</li>
-						<li>Ruby</li>
-						<li>HTML</li>
-						<li>CSS</li>
+						<li>Test driven development</li>
 					</ul>
 				</div>
 				<div>
 					<ul>
+						<li>Pair programming</li>
 						<li>Working on a website</li>
-						<li>I have good customer care experience</li>
-						<li>SQL</li>
 						<li>small amounts of AWS</li>
 						<li>Customer service certified</li>
 					</ul>
 				</div>
 				<div>
 					<ul>
-						<li>Test driven development</li>
-						<li>Pair programming</li>
+						<li>Blueprint reading</li>
 						<li>CNC Lathe Operation</li>
 						<li>Forklift certified</li>
 						<li>OSHA certified</li>
-						<li>Blueprint reading</li>
 					</ul>
 				</div>
 			</div>
 
 			<div>
 				<h1>Professional Development</h1>
+
+				<h2 className="underline_center_top">Google IT Support Fundamentals Certification</h2>
+				<p className="text_center">
+					This certification program was very diverse, contained many different subjects such as scripting simple automation and formating regular expressions. It covered Customer care and being thorough in your questions while keeping your client feeling safe. The course continued to cover things like various computer hardware. Afterwards what was included was many of the various details of computer networking covering everything from an ethernet frame to many of the networking protocols. It also covered many of he different IT toold such as Active directory, mailing servers, and various topics such as DNS and DHCP. They covered roles of a System Adminitrator and covered many areas of making you think how to set up an IT infrastructure and best practices with Stocking devices for employees. Lastly they touched heavily on IT Security from Password best practices to many security protocols and various encryption methods.
+				</p>
+
 				<h2 className="underline_center_top">Launch academy</h2>
 				<p className="text_center">
 					Launch academy was training that included 18 weeks of immersive full
@@ -84,6 +87,18 @@ export const Resume = () => {
 
 			<div>
 				<h1 className="top_pad">Past Jobs</h1>
+
+				<h3>OCT 2022</h3>
+				<h2 className="res_header2">
+					Help Desk / Massasoit Community College, Brockton MA
+				</h2>
+				<ul>
+					<li>Support with basic over the phone troubleshooting</li>
+					<li>Experience with Accurate Ticket assigning</li>
+					<li>Some hands on hardware support striping hard drives and component swapping</li>
+					<li>Occasionally taking on campus calls to offices</li>
+				</ul>
+
 				<h3>MAY 2021</h3>
 				<h2 className="res_header2">
 					CNC Machine Operator / AMI, Middleboro MA
@@ -164,31 +179,6 @@ export const Resume = () => {
 					<li>food preperation</li>
 					<li>using customer input to output desired results</li>
 				</ul>
-			</div>
-
-			<div>
-				<h1>Education</h1>
-
-				<h3>MAY 2020</h3>
-				<h2 className="res_header2">
-					Forever Student / Massasoit, Brockton MA
-				</h2>
-				<p>
-					Whenever I have free time I like to take courses to learn new things
-					I've taken many different general studies courses, along with a
-					computer hardware class and some java classes and plan on continuing
-					to work towards (a) Degree(s)
-				</p>
-			</div>
-
-			<div>
-				<h3>
-					I am also working on certification on different certifications, I'm
-					very interesting in getting my Comptia A+, and the CCNA right now. But
-					I don't plan on stopping there more to come im interested in Full
-					stack Development and the field of Network Engineering, I'm very
-					interested in computers hardware and software alike.
-				</h3>
 			</div>
 		</div>
 	);
